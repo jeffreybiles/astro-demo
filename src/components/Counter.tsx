daca1c72caad62d43353db2305b32b0046825ddb
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-04 11:53:49 -0600

    React + Vue + Nanostores

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { useStore } from '@nanostores/react';
+import { count } from '../count';
+
+export default function Counter() {
+  // Declare a new state variable, which we'll call "count"
+  const $count = useStore(count);
+
+  return (
+    <div>
+      <p>You clicked {$count} times</p>
+      <button onClick={() => count.set($count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{$count}}
+  </div>
+  <button @click="() => count.set($count + 1)">
+    + 1
+  </button>
+</template>
+
+<script setup lang="ts">
+import { count } from '../count'
+import { useStore } from '@nanostores/vue'
+
+import { ref } from 'vue';
+
+const $count = useStore(count)
+</script>
+
+<style scoped>
+
+</style>
